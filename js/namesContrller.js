0b8ecb788d92faa3b86f1bde99f7f489766b9adc
dUXINBrj <duxin.sky@gmail.com>
2017-01-18 11:50:12 +0800

    201701181150

---
+++ js/namesContrller.js
@@ -0,0 +1,7 @@
+angular.module("fliter",[]).controller("nameCtrl",function($scope){
+	$scope.names=[
+	{name:'Jani',country:'Norway'},
+	{name:'Hege',country:'Sweden'},
+	{name:'Kai',country:'Denmark'}
+	];
+});
